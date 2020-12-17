87962d52af5325f003bdfde69d710c0b4a6247fb
Javier Moreno <mq2880@gmail.com>
2020-12-17 11:34:49 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Module 1: Overview of Service and Cloud Technologies
+
+# Lab: Exploring the Work Environment
+
+1. **Nombres y apellidos:** Francisco Javier Moreno Quevedo
+2. **Fecha:** 17/12/2020
+3. **Resumen del Ejercicio:**  Crear el controlador de la aplicacion
+4. **Dificultad o problemas presentados y como se resolvieron:** Ninguna
+
+- Ejercicio 3: Creating a Web API Class
+
+  - creamos el controlador FlightsController
+
+  - Añadimos el constructor y las acciones Get y post
+  
+    
+
+  ![](./img/Captura1.jpg)
+
---
+++ Starter/BlueYonder.Flights/Controllers/FlightsController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using BlueYonder.Flights.Models;
+
+namespace BlueYonder.Flights.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FlightsController : ControllerBase
+    {
+        private readonly FlightsContext _context;
+
+        public FlightsController(FlightsContext context)
+        {
+            _context = context;
+        }
+        
+        // GET api/flights
+        [HttpGet]
+        public IEnumerable<Flight> Get()
+        {
+            return _context.Flights.ToList();
+        }
+
+        // POST api/flights
+        [HttpPost]
+        public IActionResult Post([FromBody] Flight flight)
+        {
+            _context.Flights.Add(flight);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(Get), flight.Id);
+        }
+    }
+
+}
